64ce5925390638790470b4bb0fe5a2e348376afd
Dario Formoso <88165956+darioformoso@users.noreply.github.com>
2024-11-20 12:03:51 -0500

    sections top nav

---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'top-navigation',
+  templateUrl: 'navigation.component.html',
+  styleUrls: ['navigation.component.scss']
+})
+
+export class TopNavigationComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+}
