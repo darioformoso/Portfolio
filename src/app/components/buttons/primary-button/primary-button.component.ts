bb5567e0d48d11c63f45341a94a33f185af81f4c
darioformoso <88165956+darioformoso@users.noreply.github.com>
2024-11-26 19:28:02 -0500

    home page
    home page redesign
    updated button
    changed theme back to italian

--- src/app/components/buttons/primary-button/primary-button.component.ts
+++ src/app/components/buttons/primary-button/primary-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'primary-button',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['primary-button.component.scss'],
 })
 export class PrimaryButton implements OnInit {
+  @Input() public alternativeText?: string;
   constructor() {}
 
   public ngOnInit() {}
