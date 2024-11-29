94ff99ace9f449fba4aaa2482486032a3c8104b6
darioformoso <88165956+darioformoso@users.noreply.github.com>
2024-11-29 18:18:00 -0500

    created project cards
    created project cards

--- src/app/sections/projects/project-card/project-card.component.ts
+++ src/app/sections/projects/project-card/project-card.component.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ProjectInfo } from '../projects.component';
 
 @Component({
   selector: 'project-card',
   templateUrl: 'project-card.component.html',
   styleUrls: ['project-card.component.scss'],
 })
-export class ProjectCardComponent implements OnInit {
-  constructor() {}
-
-  public ngOnInit() {}
+export class ProjectCardComponent {
+  @Input({ required: true }) public project?: ProjectInfo;
 }
--- src/app/sections/projects/projects.component.ts
+++ src/app/sections/projects/projects.component.ts
@@ -1,12 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+export type ProjectInfo = {
+  description: string;
+  image: string;
+  name: string;
+  link: string;
+};
+
+export type ProjectsInfo = ProjectInfo[];
 
 @Component({
   selector: 'projects',
   templateUrl: 'projects.component.html',
   styleUrls: ['projects.component.scss'],
 })
-export class ProjectsComponent implements OnInit {
-  constructor() {}
-
-  public ngOnInit() {}
+export class ProjectsComponent {
+  public projects: ProjectsInfo = [
+    {
+      description: 'test',
+      image: 'https://unsplash.it/400/200',
+      name: 'Project',
+      link: '',
+    },
+    {
+      description: 'test',
+      image: 'https://unsplash.it/400/200',
+      name: 'Project',
+      link: '',
+    },
+    {
+      description: 'test',
+      image: 'https://unsplash.it/400/200',
+      name: 'Project',
+      link: '',
+    },
+    {
+      description: 'test',
+      image: 'https://unsplash.it/400/200',
+      name: 'Project',
+      link: '',
+    },
+  ];
 }
